8e35686e1e1db1e5fac9cf6b8b78e7c0cacf94d7
jacksparow-222 <smail.al.harrak@gmail.com>
2025-12-13 01:39:15 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Services from '../components/Services'
 import Portfolio from '../components/Portfolio'
@@ -5,22 +6,30 @@ import Pricing from '../components/Pricing'
 import Testimonials from '../components/Testimonials'
 import Contact from '../components/Contact'
 import InstantDemo from '../components/InstantDemo'
-import LanguageToggle from '../components/LanguageToggle'
 import { FloatingWhatsApp, LanguageWrapper } from '../components/Extras'
 
 export default function Page() {
   return (
     <LanguageWrapper>
-      <LanguageToggle />
+      <Navbar />
       <FloatingWhatsApp />
       <main>
         <Hero />
-        <InstantDemo />
-        <Services />
-        <Portfolio />
-        <Pricing />
-        <Testimonials />
-        <Contact />
+        <section id="services">
+          <Services />
+        </section>
+        <section id="portfolio">
+          <Portfolio />
+        </section>
+        <section id="pricing">
+          <Pricing />
+        </section>
+        <section id="testimonials">
+          <Testimonials />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
       </main>
     </LanguageWrapper>
   )
