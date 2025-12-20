b8bf55ca3afc8f34affaccf85291c024fdb5afd7
jacksparow-222 <smail.al.harrak@gmail.com>
2025-12-20 15:05:13 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
   publisher: 'Oujda Digital Studio',
   verification: {
     google: "JdmunYkh8JO98lPqy1R-dqi3y1u-O_ifqxKhF2yyCvU",
-  }
+  },
   
   // ROBOTS
   robots: {
